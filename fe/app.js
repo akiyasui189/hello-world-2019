fbc9d2d726d0a87af6e2f5c551cf0fbecd3baea1
Akihiko Yasui <akiyasui@AkiYasuiMBP.local>
2019-08-12 14:05:30 +0900

    add chat application

--- fe/app.js
+++ fe/app.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const http = require('http').createServer(app);
+const io = require('socket.io').listen(http);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+io.on('connection', function(socket) {
+    // start connection
+    console.log('a user connected');
+    // receive message
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    // disconnect
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'))
